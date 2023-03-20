64a267168adeafa19dd36413d3a9a6a4e7c596e6
azamat Khadikov <khadikovazamat@gmail.com>
2023-03-20 13:15:05 -0600

    saving the assignment!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] M;
+Console.WriteLine("Enter an array");
+string stroka = Console.ReadLine();
+M = stroka.Split(',');
+var result = new string[M .Length];
+var realSize = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
